d1fd4428c9b7f05d1986c9e2bd041858a63b02ba
KhweziN <khwezisandi@gmail.com>
2025-02-11 23:38:41 +0200

    added cmake files

---
+++ build/CMakeFiles/Pathfinder.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pathfinder.dir/Grid.cpp.o"
+  "CMakeFiles/Pathfinder.dir/Grid.cpp.o.d"
+  "CMakeFiles/Pathfinder.dir/Pathfinder.cpp.o"
+  "CMakeFiles/Pathfinder.dir/Pathfinder.cpp.o.d"
+  "Pathfinder"
+  "Pathfinder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pathfinder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
